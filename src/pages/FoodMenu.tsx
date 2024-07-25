7c4aae3624ca395445f89ce6a73520d994438d18
bartox7777 <bartosz.zelek@protonmail.com>
2024-07-26 00:17:58 +0200

    #6 add basic filters

--- src/pages/FoodMenu.tsx
+++ src/pages/FoodMenu.tsx
@@ -1,11 +1,53 @@
-import { Container, Grid, Divider, Typography } from "@mui/material";
+import {
+  Container,
+  Grid,
+  Divider,
+  Typography,
+  List,
+  ListItem,
+  Autocomplete,
+  Chip,
+  TextField,
+  ListItemText,
+  ListItemSecondaryAction,
+  ListSubheader,
+  Rating,
+  Slider,
+} from "@mui/material";
 
 import { useState } from "react";
 
 import { CategorySelector } from "../components/FoodMenu/CategorySelector/CategorySelector";
 import { DishesList } from "../components/FoodMenu/DishesList/DishesList";
 export const Menu = () => {
+  const [minStars, setMinStars] = useState<number>(1);
+  const [minMaxPrice, setMinMaxPrice] = useState<number[]>([0, 1000]);
   const [category, setCategory] = useState<string>("Pizza");
+  const [excludedIngridients, setExcludedIngredients] = useState<string[]>([]);
+
+  const handlePriceFilter = (
+    _: Event,
+    newValue: number | number[],
+    activeThumb: number
+  ) => {
+    if (!Array.isArray(newValue)) {
+      return;
+    }
+
+    if (newValue[1] - newValue[0] < 10) {
+      if (activeThumb === 0) {
+        const clamped = Math.min(newValue[0], 200 - 10);
+        setMinMaxPrice([clamped, clamped + 10]);
+      } else {
+        const clamped = Math.max(newValue[1], 10);
+        setMinMaxPrice([clamped - 10, clamped]);
+      }
+    } else {
+      setMinMaxPrice(newValue);
+    }
+  };
+
+  const ingredients = ["Ser", "Szynka", "Pieczarki", "Ananas", "Oliwki"];
 
   return (
     <Container sx={{ mt: 3 }} maxWidth="xl">
@@ -23,8 +65,68 @@ export const Menu = () => {
           </Grid>
           <Grid item xs={12}>
             <Grid container>
-              <Grid item xs={3}>
-                Site navigation
+              <Grid
+                item
+                xs={3}
+                sx={{ p: 3, borderRight: 1, borderColor: "divider" }}
+              >
+                <Typography
+                  variant="h5"
+                  sx={{
+                    textAlign: "center",
+                  }}
+                >
+                  Filtry
+                </Typography>
+                <List>
+                  {/* filters like price, rating, ingridients to exclude */}
+                  <ListSubheader>Cena</ListSubheader>
+                  <ListItem sx={{ mt: 4 }}>
+                    <Slider
+                      value={minMaxPrice}
+                      onChange={handlePriceFilter}
+                      valueLabelDisplay="auto"
+                      getAriaValueText={(num: number) => `${num} zł`}
+                      disableSwap
+                      min={0}
+                      max={200}
+                    />
+                  </ListItem>
+                  <Divider />
+
+                  <ListSubheader>Minimalna ocena</ListSubheader>
+                  <ListItem>
+                    <Rating
+                      value={minStars}
+                      onChange={(_, value) => {
+                        setMinStars(value ?? 1);
+                      }}
+                    />
+                  </ListItem>
+                  <Divider />
+
+                  <ListSubheader>Składniki do wykluczenia</ListSubheader>
+                  <ListItem>
+                    <Autocomplete
+                      multiple
+                      value={excludedIngridients}
+                      onChange={(_, newValue) => {
+                        setExcludedIngredients([...newValue]);
+                      }}
+                      options={ingredients}
+                      renderTags={(tagValue, getTagProps) =>
+                        tagValue.map((option, index) => {
+                          const { key, ...tagProps } = getTagProps({ index });
+                          return (
+                            <Chip key={key} label={option} {...tagProps} />
+                          );
+                        })
+                      }
+                      style={{ width: 500 }}
+                      renderInput={(params) => <TextField {...params} />}
+                    />
+                  </ListItem>
+                </List>
               </Grid>
               <Grid item xs={9}>
                 <DishesList category={category} />
