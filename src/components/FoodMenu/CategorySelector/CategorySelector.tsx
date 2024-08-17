e08af0ee5443ac8310712fdc1b28398c25fbd7a9
bartox7777 <bartosz.zelek@protonmail.com>
2024-08-18 00:43:26 +0200

    fetch categories from api

--- src/components/FoodMenu/CategorySelector/CategorySelector.tsx
+++ src/components/FoodMenu/CategorySelector/CategorySelector.tsx
@@ -1,6 +1,3 @@
-import { Box } from "@mui/material";
-
-import { Swiper, SwiperSlide } from "swiper/react";
 import {
   FreeMode,
   Pagination,
@@ -8,30 +5,69 @@ import {
   Autoplay,
   Mousewheel,
 } from "swiper/modules";
-
-import { CategoryCard } from "./CategoryCard";
-
 import "swiper/css";
 import "swiper/css/pagination";
+import { Box, Skeleton } from "@mui/material";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import { useState, useEffect } from "react";
+
+import api from "../../../utils/api";
+import { CategoryCard } from "./CategoryCard";
 
 interface CategorySelectorProps {
   setCategory: (category: string) => void;
 }
 
+const fetchCategories = async () => {
+  const response = await api.get("/api/categories/all");
+  if (response.status === 200) {
+    return response.data;
+  } else {
+    console.error(response);
+    return [];
+  }
+};
+
 export const CategorySelector: React.FC<CategorySelectorProps> = ({
   setCategory,
 }) => {
-  const categories: [string, string][] = [
-    ["Pizza", "icons8-pizza.svg"],
-    ["Spaghetti", "icons8-spaghetti.svg"],
-    ["Sałatki", "icons8-salad.svg"],
-    ["Burgery", "icons8-burger.svg"],
-    ["Desery", "icons8-dessert.svg"],
-    ["Napoje", "icons8-coffee-cup.svg"],
-    ["Sushi", "icons8-sushi.svg"],
-    ["Zupy", "icons8-noodles.svg"],
-    ["Kanapki", "icons8-sandwich.svg"],
-  ];
+  const [categories, setCategories] = useState<[string, string][]>([]);
+
+  useEffect(() => {
+    fetchCategories().then((data: { id: number; name: string }[]) => {
+      const categories_imgs = [
+        "icons8-pizza.svg",
+        "icons8-spaghetti.svg",
+        "icons8-salad.svg",
+        "icons8-burger.svg",
+        "icons8-dessert.svg",
+        "icons8-coffee-cup.svg",
+        "icons8-sushi.svg",
+        "icons8-noodles.svg",
+        "icons8-sandwich.svg",
+      ];
+
+      setCategories(
+        data
+          .toReversed()
+          .map((json, index: number) => [json.name, categories_imgs[index]])
+      );
+    });
+  }, []);
+
+  // let categories: [string, string][] = [
+  //   ["Pizza", "icons8-pizza.svg"],
+  //   ["Spaghetti", "icons8-spaghetti.svg"],
+  //   ["Sałatki", "icons8-salad.svg"],
+  //   ["Burgery", "icons8-burger.svg"],
+  //   ["Desery", "icons8-dessert.svg"],
+  //   ["Napoje", "icons8-coffee-cup.svg"],
+  //   ["Sushi", "icons8-sushi.svg"],
+  //   ["Zupy", "icons8-noodles.svg"],
+  //   ["Kanapki", "icons8-sandwich.svg"],
+  // ];
+
   return (
     <Box>
       <Swiper
@@ -67,6 +103,21 @@ export const CategorySelector: React.FC<CategorySelectorProps> = ({
         }}
         freeMode={true}
       >
+        {categories.length === 0 &&
+          Array(6)
+            .fill(null)
+            .map((_, index) => (
+              <SwiperSlide key={index}>
+                <Skeleton
+                  height={200}
+                  width={110}
+                  sx={{
+                    my: 5,
+                    mx: 1,
+                  }}
+                />
+              </SwiperSlide>
+            ))}
         {categories.map(([category, icon]) => (
           <SwiperSlide
             key={category}
